d5e25968ae29783bb098d8e3ba8ccd32adae96ac
CMAnalytics <john.coene@conew.com>
2016-02-24 15:28:24 +0800

    init

---
+++ R/plot2earth.R
@@ -0,0 +1,119 @@
+#' plot2earth
+#' 
+#' @description
+#' 
+#' Plots tweets location and created time on Google Earth.
+#' 
+#' @param df Required. data.frame of tweets to plot.
+#' @param longitude Required. Column holding longitude.
+#' @param latitude Required. Column holding latitude.
+#' @param date.time Required. Column including creation time of tweets; 
+#' \code{POSIXct} format
+#' @param shape, optional. Shape to use for points. Defaults to 
+#' \code{http.../icon18.png}
+#' @param labels Required. Column holding labels.
+#' @param size Required. Column holding desired size of points.  
+#' @param colour, optional. Column name that determines colours of points.
+#' @param colour.scale, optional, defaults to \code{plotKML::SAGA_pal[[1]]}. 
+#' can be pallettes from R_pal (\code{data(R_pal)}), SAGA_pal (see default), 
+#' from \code{RColorBrewer} (see example) or a vector of colors (hex) 
+#' i.e.(\code{c("#EE6A50", "#E8F9FE", "#6495ED")})
+#' @param open, optional. Whether to open file in Google Earth. 
+#' Defaults to \code{TRUE}.
+#' @param file.name Name of kml or kmz file, defaults to \code{tweets2earth.kml}
+#' @param folder.name Name of folder where to save the kml or kmz file, 
+#' defaults to \code{getwd}
+#' @param ... Arguments to pass to \code{kml} function. 
+#' See \code{\link[plotKML]{kml}}
+#' @param kmz Whether to also save the file as \code{.kmz}, defaults to 
+#' \code{FALSE}
+#' 
+#' @details
+#' 
+#' Takes data.frame of tweets and plots coordinates on Google Earth, also 
+#' includes time variable (\code{date.time}). Choose \code{size}, 
+#' \code{colour} and \code{labels} of your liking.
+#' 
+#' @return 
+#' 
+#' Writes kml file and, optionally, opens it in Google Earth 
+#' (\code{open = TRUE}).
+#' 
+#' @examples
+#' 
+#' \dontrun{
+#' library(twitteR)
+#' 
+#' # setup oauth 
+#' setup_twitter_oauth(consumer_key, consumer_secret, access_token=your_token, 
+#'                     access_secret=your_secret)
+#' 
+#' # search twitter
+#' tweets <- searchTwitter("rstats", n = 2000, lang= "en",
+#'                         geocode="34.052235,-118.243683,100mi")
+#' 
+#' # unlist
+#' tw.df <- do.call("rbind", lapply(tweets, as.data.frame))
+#' 
+#' plot2earth(tw.df, "longitude", "latitude", "created", 
+#'            "screenName", "retweetCount", "favorited", 
+#'            colour.scale = colour.scale = RColorBrewer::brewer.pal(8, "BuPu"),
+#'            file.name = "rstats.kml")
+#' }
+#' 
+#' @author John Coene, \email{jcoenep@@gmail.com}
+#' 
+#' @seealso \\code{\link{stream2earth}}
+#' 
+#' @export
+plot2earth <- function(df, longitude, latitude, date.time, labels, size, colour,
+                       colour.scale = plotKML::SAGA_pal[[1]], 
+                       shape = "http://maps.google.com/mapfiles/kml/pal2/icon18.png",
+                       open = TRUE, kmz = FALSE, file.name = "tweets2earth.kml",
+                       folder.name = getwd(), ...) {
+  
+  if(missing(df)) stop("must pass df")
+  if(missing(longitude)) stop("must pass longitude")
+  if(missing(latitude)) stop("must pass latitude")
+  if(missing(date.time)) stop("must pass date.time")
+  if(missing(labels)) stop("must pass labels")
+  if(missing(size)) stop("must pass size")
+  if(missing(colour)) stop("must pass colour")
+  
+  df <- df[,c(longitude, latitude, date.time, labels, size, colour)]
+  names(df) <- c("longitude", "latitude", "date_time", "labels", "size",
+                 "colour")
+  
+  # Remove unknown locations
+  df <- df[!is.na(df$longitude),]
+  
+  # Change long & lat class
+  df$longitude <- as.numeric(df$longitude)
+  df$latitude <- as.numeric(df$latitude)
+  
+  # format the time column:
+  df$date_time <- as.POSIXct(df$date_time, format="%Y-%m-%dT%H:%M:%SZ")
+  
+  # take unique
+  df <- df[!duplicated(df[,c("longitude", "latitude", "labels")]),]
+  
+  # create a STIDF object:
+  sp <- sp::SpatialPoints(df[,c("longitude","latitude")])
+  sp::proj4string(sp) <- sp::CRS("+proj=longlat +datum=WGS84")
+  
+  tweets2earth <- spacetime::STIDF(sp, time = df$date_time, 
+                                   data = df[,c("labels","size", "colour")])
+  
+  # write to a KML file:
+  shape <- shape
+  
+  plotKML::kml(tweets2earth, dtime = 24*3600, size = size, shape = shape, 
+               labels = labels, colour_scale = colour.scale, colour = colour,
+               kmz = kmz, file.name = file.name, folder.name = folder.name)
+  
+  # opens KML file on Google Earth 
+  if(open == TRUE) system("open tweets2earth.kml")
+  
+  return(tweets2earth)
+  
+}
---
+++ R/stream2earth.R
@@ -0,0 +1,155 @@
+#' stream2earth
+#' 
+#' @description
+#' 
+#' Streams tweets' location and time to Google Earth.
+#' 
+#' @param search_term, required. Search to use in API call.
+#' @param oauth, required. Your oauth token as used in streamR package. 
+#' See examples.
+#' @param duration, requried. Number of seconds to stream, defaults to 
+#' \code{3600}.
+#' @param window, required. Duration of each streaming window in seconds. 
+#' Defaults to \code{5}.
+#' @param labels, requried. Labels to use on Google Earth. must be inherited 
+#' from \code{\link[streamR]{filterStream}} (see details)
+#' @param shape, required. Shape to use on Google Earth. Defaults to 
+#' \code{.../icon18.png}.
+#' @param size, required. Size of shape. must be inherited from 
+#' \code{\link[streamR]{filterStream}} (see details).
+#' @param ... Optional arguments to pass to streamR filterStream function.
+#' 
+#' @return Opens Google Earth and plots tweet location and created time in 
+#' near-real time.
+#' 
+#' @examples
+#' \dontrun{
+#' # OAuth setup - from App (http://apps.twitter.com/)
+#' requestURL <- "https://api.twitter.com/oauth/request_token"
+#' accessURL <- "https://api.twitter.com/oauth/access_token"
+#' authURL <- "https://api.twitter.com/oauth/authorize"
+#' my_oauth <- OAuthFactory$new(consumerKey = "your_consumer_key", 
+#'                              consumerSecret = "your_consumer_secret",
+#'                              requestURL = requestURL, accessURL = accessURL, 
+#'                              authURL = authURL)
+#' 
+#' # download cacert.pem
+#' download.file(url="http://curl.haxx.se/ca/cacert.pem", destfile="cacert.pem")
+#' 
+#' # Register OAuth
+#' my_oauth$handshake(cainfo = system.file("CurlSSL", "cacert.pem", 
+#'                                         package = "RCurl"))
+#' 
+#' #Run stream for 2 days with windows of 7 seconds looking for keyword #rstats
+#' stream2Earth("rstats", my_oauth, "screen_name", "retweet_count", 
+#'              duration = 2, window = 7)
+#' }
+#' 
+#' @details
+#' \code{labels}, \code{size} 
+#' parameters must be inherited from \code{\link[streamR]{filterStream}}. 
+#' 
+#' Available labels and size parameters are;
+#' 
+#' \itemize{
+#' \item \code{text}
+#' \item \code{favorited}
+#' \item \code{id_str}
+#' \item \code{source}
+#' \item \code{created_at}
+#' \item \code{in_reply_to_user_id_str}
+#' \item \code{listed_count}
+#' \item \code{location}
+#' \item \code{description}
+#' \item \code{user_created_at}
+#' \item \code{followers_count}
+#' \item \code{protected}
+#' \item \code{names}
+#' \item \code{user_lang}
+#' \item \code{friends_count}
+#' \item \code{time}
+#' \item \code{retweet_count}
+#' \item \code{truncated}
+#' \item \code{in_reply_to_screen_name}
+#' \item \code{retweeted}
+#' \item \code{in_reply_to_status_id_str}
+#' \item \code{lang}
+#' \item \code{verified}
+#' \item \code{user_id_str}
+#' \item \code{geo_enabled}
+#' \item \code{statuses_count}
+#' \item \code{favourites_count}
+#' \item \code{user_url}
+#' \item \code{time_zone}
+#' \item \code{utc_offset}
+#' \item \code{screen_name}
+#' }
+#' 
+#' @author Jabber Cruncher, \email{john.coene@@jabber-cruncher.com}
+#' 
+#' @seealso \code{\link{plot2earth}}
+#' 
+#' @export
+stream2earth <- function(search_term, oauth, labels, size, duration = 3600,
+                         window = 5, 
+                         shape = "http://maps.google.com/mapfiles/kml/pal2/icon18.png",
+                         ...) {
+  if (class(oauth) != "OAuth") {
+    stop("oauth not of class OAuth")
+  } else if (class(duration) != "numeric") {
+    stop ("Duration parameter not of class numeric")
+  } else if (class(window) != "numeric"){
+    stop ("window must be of class numeric")
+  } else if (missing(search_term)) {
+    stop ("missing search_term")
+  } else if (missing(oauth)) {
+    stop("missing oauth")
+  }
+  end.date <- Sys.time() + duration
+  tw.df <- data.frame()
+  
+  while (Sys.time() < end.date){
+    current.time <- format(Sys.time(), "%Y_%m_%d_%H_%M")
+    json.file <- paste(search_term, "_", current.time, ".json", sep="")
+    streamR::filterStream(file=json.file, track=search_term, oauth=oauth, 
+                          timeout=window, ...)
+    
+    if (file.info(json.file)$size > 0) {
+      
+      df <- streamR::parseTweets(json.file)
+      df <- df[complete.cases(df), ]
+      tw.df <- as.data.frame(rbind(tw.df, df))
+      
+    } else {
+      
+      print ("no tweets found")
+      
+      }
+    
+    if (length(df$place_lon) >= 1) {
+      # build df
+      tw.df <- tw.df[,c("place_lon", "place_lat", "created_at", size, labels)]
+      names(tw.df) <- c("place_lon", "place_lat", "created_at","size", "labels")
+      
+      # get shape
+      shape <- shape
+      
+      # fix time
+      tw.df$time <- substr(tw.df$created_at, 12, 19)
+      tw.df$time <- as.POSIXct(strptime(tw.df$time, "%H:%M:%S"))
+      
+      # plot
+      sp <- sp::SpatialPoints(tw.df[,c("place_lon","place_lat")])
+      sp::proj4string(sp) <- sp::CRS("+proj=longlat +datum=WGS84")
+      df_st <- spacetime::STIDF(sp, time = tw.df$time, data = tw.df[,c("labels","size")])
+      
+      plotKML::plotKML(df_st, dtime = 24*3600, points_names=tw.df$labels, 
+              LabelScale = .4)
+    } else {
+      
+      Sys.sleep(1)
+      print("no geo-located tweets found, restarting stream")
+      
+    }
+  }
+}
